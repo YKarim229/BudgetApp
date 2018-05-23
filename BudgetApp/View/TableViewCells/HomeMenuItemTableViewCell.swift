9c6a0df8ba6de80e1d0c20eb2ca880c05dd9c8ea
Pawel Milek <PawelMilek@Apples-MacBook-Pro.local>
2018-05-22 21:49:13 -0500

    Create initial HomeMenuItemTableViewCell xib

---
+++ BudgetApp/View/TableViewCells/HomeMenuItemTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  HomeMenuItemTableViewCell.swift
+//  BudgetApp
+//
+//  Created by Pawel Milek on 22/05/2018.
+//  Copyright © 2018 PHP AFRIQUE. All rights reserved.
+//
+
+import UIKit
+
+class HomeMenuItemTableViewCell: UITableViewCell {
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }    
+}
